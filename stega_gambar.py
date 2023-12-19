0361f9a158717bf326eda33e4c0df9d16dbd0a87
embundutalaksmana <embun20ti@mahasiswa.pcr.ac.id>
2023-12-20 01:18:42 +0700

    21d commit

--- stega_gambar.py
+++ stega_gambar.py
@@ -1,106 +1,65 @@
 import streamlit as st
 from PIL import Image
+import io
 
-def genData(data):
-    newd = [format(ord(i), '08b') for i in data]
-    return newd
+def encode(img, message):
+    pixels = list(img.getdata())
+    binary_message = ''.join(format(ord(char), '08b') for char in message) + '1111111111111110'
 
-def modPix(pix, data):
-    datalist = genData(data)
-    lendata = len(datalist)
-    imdata = iter(pix)
+    data_index = 0
+    for i in range(len(pixels)):
+        pixel = list(pixels[i])
+        for j in range(3):
+            if data_index < len(binary_message):
+                pixel[j] = pixel[j] & ~1 | int(binary_message[data_index])
+                data_index += 1
 
-    for i in range(lendata):
-        pix = [value for value in imdata.__next__()[:3] +
-                                 imdata.__next__()[:3] +
-                                 imdata.__next__()[:3]]
+        pixels[i] = tuple(pixel)
 
-        for j in range(8):
-            if (datalist[i][j] == '0' and pix[j] % 2 != 0):
-                pix[j] -= 1
-            elif (datalist[i][j] == '1' and pix[j] % 2 == 0):
-                pix[j] += 1
+    encoded_img = Image.new(img.mode, img.size)
+    encoded_img.putdata(pixels)
+    return encoded_img
 
-        if (i == lendata - 1):
-            if (pix[-1] % 2 == 0):
-                pix[-1] += 1
+def decode(img):
+    pixels = list(img.getdata())
+    binary_message = ''
+
+    for pixel in pixels:
+        for j in range(3):
+            binary_message += str(pixel[j] & 1)
+
+    delimiter_index = binary_message.find('1111111111111110')
+    binary_message = binary_message[:delimiter_index]
+
+    message = ''.join(chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8))
+    return message
+
+# Streamlit App
+st.title("Steganografi Gambar dengan LSB")
+
+option = st.selectbox("Pilih Operasi", ["Encode", "Decode"])
+
+if option == "Encode":
+    st.subheader("Sisipkan Pesan ke dalam Gambar")
+    image_file = st.file_uploader("Pilih Gambar", type=["jpg", "jpeg", "png"])
+    message = st.text_area("Masukkan Pesan yang Akan Disisipkan")
+
+    if st.button("Encode"):
+        if image_file is not None and message != "":
+            img = Image.open(io.BytesIO(image_file.read()))
+            encoded_img = encode(img, message)
+            st.image(encoded_img, caption="Gambar Hasil Encode", use_column_width=True)
         else:
-            if (pix[-1] % 2 != 0):
-                pix[-1] -= 1
+            st.warning("Silakan pilih gambar dan masukkan pesan terlebih dahulu.")
 
-        pix = tuple(pix)
-        yield pix[0:3]
-        yield pix[3:6]
-        yield pix[6:9]
+elif option == "Decode":
+    st.subheader("Ekstrak Pesan dari Gambar")
+    image_file = st.file_uploader("Pilih Gambar yang Telah Disisipkan Pesan", type=["jpg", "jpeg", "png"])
 
-def encode_enc(newimg, data):
-    w = newimg.size[0]
-    (x, y) = (0, 0)
-
-    for pixel in modPix(newimg.getdata(), data):
-        newimg.putpixel((x, y), pixel)
-        if (x == w - 1):
-            x = 0
-            y += 1
+    if st.button("Decode"):
+        if image_file is not None:
+            img = Image.open(io.BytesIO(image_file.read()))
+            decoded_message = decode(img)
+            st.success(f"Pesan yang diekstrak: {decoded_message}")
         else:
-            x += 1
-
-def steganography_app():
-    st.title("Steganography with Image B & W")
-
-    operation = st.sidebar.selectbox("Select Operation", ["Encode", "Decode","About"])
-
-    if operation == "Encode":
-        st.subheader("Encode Data into Image")
-        image_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
-        data = st.text_area("Enter Data to be Encoded")
-
-        if st.button("Encode"):
-            if image_file is not None and data != "":
-                image = Image.open(image_file)
-                new_image = image.copy()
-                encode_enc(new_image, data)
-
-                st.image(new_image, caption="Encoded Image", use_column_width=True)
-            else:
-                st.warning("Please upload an image and enter data to be encoded.")
-
-    elif operation == "Decode":
-        st.subheader("Decode Data from Image")
-        image_file = st.file_uploader("Upload Encoded Image", type=["jpg", "jpeg", "png"])
-
-        if st.button("Decode"):
-            if image_file is not None:
-                image = Image.open(image_file)
-                decoded_data = decode_enc(image)
-                st.success(f"Decoded Data: {decoded_data}")
-            else:
-                st.warning("Please upload an encoded image.")
-
-    elif operation == "About":
-        st.subheader("Kelompok Sekian | 4 TI E")
-        st.code(f"""
-                    Embun Duta Laksmana
-
-                    Jessen Wind Lim)
-                    """)
-       
-def decode_enc(image):
-    data = ''
-    img_data = iter(image.getdata())
-
-    while True:
-        pixels = [value for value in img_data.__next__()[:3] +
-                                  img_data.__next__()[:3] +
-                                  img_data.__next__()[:3]]
-
-        bin_str = ''.join('0' if i % 2 == 0 else '1' for i in pixels[:8])
-        data += chr(int(bin_str, 2))
-
-        if pixels[-1] % 2 != 0:
-            break
-
-    return data
-
-if __name__ == "__main__":
-    steganography_app()
+            st.warning("Silakan pilih gambar terlebih dahulu.")
