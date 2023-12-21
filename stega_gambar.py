777486501387803dae7d280e385c88cf5ef68afe
embundutalaksmana <embun20ti@mahasiswa.pcr.ac.id>
2023-12-21 16:00:56 +0700

    21j commit

--- stega_gambar.py
+++ stega_gambar.py
@@ -1,63 +1,76 @@
 import streamlit as st
+import numpy as np
 from PIL import Image
 
-def lsb_encode(cover_path, secret_path, output_path):
-    cover = Image.open(cover_path).convert('RGB')
-    secret = Image.open(secret_path).convert('RGB')
+def text_to_binary(text):
+    binary_text = ''.join(format(ord(char), '08b') for char in text)
+    return binary_text
 
-    cover_data = cover.getdata()
-    secret_data = secret.getdata()
+def encode_lsb(image, secret_text):
+    binary_secret_text = text_to_binary(secret_text)
+    binary_secret_text += '1111111111111110'  # Menambahkan delimiter akhir
 
-    stego_data = []
-    for cover_pixel, secret_pixel in zip(cover_data, secret_data):
-        stego_pixel = []
-        for c, s in zip(cover_pixel, secret_pixel):
-            stego_channel = (c & 0xFE) | ((s >> 7) & 1)
-            stego_pixel.append(stego_channel)
-        stego_data.append(tuple(stego_pixel))
+    data_index = 0
+    image_data = np.array(image)
 
-    stego = Image.new('RGB', cover.size)
-    stego.putdata(stego_data)
-    stego.save(output_path)
+    for row in range(image_data.shape[0]):
+        for col in range(image_data.shape[1]):
+            for color_channel in range(image_data.shape[2]):
+                if data_index < len(binary_secret_text):
+                    pixel_value = image_data[row, col, color_channel]
+                    new_pixel_value = pixel_value & 0b11111110 | int(binary_secret_text[data_index])
+                    image_data[row, col, color_channel] = new_pixel_value
+                    data_index += 1
 
-def lsb_decode(stego_path):
-    stego = Image.open(stego_path).convert('RGB')
-    stego_data = stego.getdata()
+    return Image.fromarray(image_data.astype('uint8'))
 
-    extracted_data = []
-    for stego_pixel in stego_data:
-        extracted_pixel = 0
-        for channel in stego_pixel:
-            extracted_pixel = (extracted_pixel << 1) | (channel & 1)
-        extracted_data.append(extracted_pixel)
+def decode_lsb(image):
+    binary_secret_text = ''
+    image_data = np.array(image)
 
-    extracted_info = bytes(extracted_data)
-    return extracted_info
+    for row in range(image_data.shape[0]):
+        for col in range(image_data.shape[1]):
+            for color_channel in range(image_data.shape[2]):
+                pixel_value = image_data[row, col, color_channel]
+                binary_secret_text += str(pixel_value & 1)
+
+    delimiter_index = binary_secret_text.find('1111111111111110')
+    binary_secret_text = binary_secret_text[:delimiter_index]
+
+    # Convert binary to text
+    secret_text = ''.join(chr(int(binary_secret_text[i:i+8], 2)) for i in range(0, len(binary_secret_text), 8))
+    return secret_text
 
 def main():
-    st.title("LSB Steganography App")
-    st.sidebar.header("Settings")
+    st.title("LSB Steganography for Text in RGB Image")
 
-    # Upload cover image
-    cover_image = st.sidebar.file_uploader("Upload Cover Image", type=["jpg", "jpeg", "png"])
+    uploaded_image = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-    # Upload secret image
-    secret_image = st.sidebar.file_uploader("Upload Secret Image", type=["jpg", "jpeg", "png"])
+    if uploaded_image is not None:
+        secret_text = st.text_area("Enter the secret text:")
+        encode_button = st.button("Encode")
 
-    if cover_image and secret_image:
-        # Output path for stego image
-        output_stego_path = "stego_image.png"
+        if encode_button:
+            original_image = Image.open(uploaded_image)
+            st.image(original_image, caption="Original Image", use_column_width=True)
 
-        # Sisipkan informasi
-        lsb_encode(cover_image, secret_image, output_stego_path)
-        st.success("Informasi telah disisipkan ke dalam gambar cover.")
+            encoded_image = encode_lsb(original_image, secret_text)
+            st.image(encoded_image, caption="Encoded Image", use_column_width=True)
 
-        # Ekstrak informasi
-        extracted_info = lsb_decode(output_stego_path)
-        st.info("Informasi yang diekstrak: {}".format(extracted_info.decode('utf-8')))
+            st.success("Image encoded successfully!")
 
-        # Tampilkan gambar cover, secret, dan stego
-        st.image([cover_image, secret_image, output_stego_path], caption=["Cover Image", "Secret Image", "Stego Image"])
+        st.header("Decode Secret Text from Image")
+        uploaded_encoded_image = st.file_uploader("Upload the encoded image", type=["jpg", "jpeg", "png"])
+
+        if uploaded_encoded_image is not None:
+            decode_button = st.button("Decode")
+
+            if decode_button:
+                encoded_image = Image.open(uploaded_encoded_image)
+                st.image(encoded_image, caption="Encoded Image", use_column_width=True)
+
+                decoded_text = decode_lsb(encoded_image)
+                st.success(f"Decoded Secret Text: {decoded_text}")
 
 if __name__ == "__main__":
     main()
