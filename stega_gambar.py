20000ed4c5d9b6d12720b33b5f660c32277ced52
embundutalaksmana <embun20ti@mahasiswa.pcr.ac.id>
2023-12-21 19:36:15 +0700

    23d commit

--- stega_gambar.py
+++ stega_gambar.py
@@ -26,7 +26,7 @@ def decode(img):
     message = ''.join(chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8))
     return message
 # Streamlit App
-st.title("Steganografi Gambar RGB dengan LSB")
+st.title("Steganografi Gambar dengan LSB")
 option = st.selectbox("Pilih Operasi", ["Encode", "Decode"])
 if option == "Encode":
     st.subheader("Sisipkan Pesan ke dalam Gambar")
