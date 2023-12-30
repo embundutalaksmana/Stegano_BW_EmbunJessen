ae6913adbbdf95707c2d80a64abc706adbf8749c
embundutalaksmana <embun20ti@mahasiswa.pcr.ac.id>
2023-12-30 15:38:03 +0700

    25 commit

--- stega_gambar.py
+++ stega_gambar.py
@@ -1,53 +1,107 @@
 import streamlit as st
+import numpy as np
 from PIL import Image
-import io
-def encode(img, message):
-    pixels = list(img.getdata())
-    binary_message = ''.join(format(ord(char), '08b') for char in message) + '1111111111111110'
-    data_index = 0
-    for i in range(len(pixels)):
-        pixel = list(pixels[i])
-        for j in range(len(pixel)):
-            if data_index < len(binary_message):
-                pixel[j] = pixel[j] & ~1 | int(binary_message[data_index])
-                data_index += 1
-        pixels[i] = tuple(pixel)
-    encoded_img = Image.new(img.mode, img.size)
-    encoded_img.putdata(pixels)
-    return encoded_img
-def decode(img):
-    pixels = list(img.getdata())
-    binary_message = ''
-    for pixel in pixels:
-        for j in range(3):
-            binary_message += str(pixel[j] & 1)
-    delimiter_index = binary_message.find('1111111111111110')
-    binary_message = binary_message[:delimiter_index]
-    message = ''.join(chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8))
-    return message
-# Streamlit App
-st.title("Steganografi Gambar dengan LSB")
-option = st.selectbox("Pilih Operasi", ["Encode", "Decode"])
-if option == "Encode":
-    st.subheader("Sisipkan Pesan ke dalam Gambar")
-    image_file = st.file_uploader("Pilih Gambar", type=["jpg", "jpeg", "png"])
-    message = st.text_area("Masukkan Pesan yang Akan Disisipkan")
-    if st.button("Encode"):
-        if image_file is not None and message != "":
-            img = Image.open(io.BytesIO(image_file.read()))
-            
-            encoded_img = encode(img, message)
-            st.image(encoded_img, caption="Gambar Hasil Encode", use_column_width=True)
+
+np.set_printoptions(threshold=np.inf)
+
+# Encoding function
+def encode(src, message, dest, password):
+    img = Image.open(src, 'r')
+    width, height = img.size
+    array = np.array(list(img.getdata()))
+
+    if img.mode == 'RGB':
+        n = 3
+    elif img.mode == 'RGBA':
+        n = 4
+
+    total_pixels = array.size // n
+
+    message += password
+    b_message = ''.join([format(ord(i), "08b") for i in message])
+    req_pixels = len(b_message)
+
+    if req_pixels > (total_pixels * 3):
+        st.error("ERROR: Need larger file size")
+    else:
+        index = 0
+        for p in range(total_pixels):
+            for q in range(0, 3):
+                if index < req_pixels:
+                    array[p][q] = int(bin(array[p][q])[2:9] + b_message[index], 2)
+                    index += 1
+
+        array = array.reshape(height, width, n)
+        enc_img = Image.fromarray(array.astype('uint8'), img.mode)
+        enc_img.save(dest)
+        st.success("Image Encoded Successfully")
+
+# Decoding function
+def decode(src, password):
+    img = Image.open(src, 'r')
+    array = np.array(list(img.getdata()))
+
+    if img.mode == 'RGB':
+        n = 3
+    elif img.mode == 'RGBA':
+        n = 4
+
+    total_pixels = array.size // n
+
+    hidden_bits = ""
+    for p in range(total_pixels):
+        for q in range(0, 3):
+            hidden_bits += (bin(array[p][q])[2:][-1])
+
+    hidden_bits = [hidden_bits[i:i + 8] for i in range(0, len(hidden_bits), 8)]
+
+    message = ""
+    hiddenmessage = ""
+    for i in range(len(hidden_bits)):
+        x = len(password)
+        if message[-x:] == password:
+            break
         else:
-            st.warning("Silakan pilih gambar dan masukkan pesan terlebih dahulu.")
-elif option == "Decode":
-    st.subheader("Ekstrak Pesan dari Gambar")
-    image_file = st.file_uploader("Pilih Gambar yang Telah Disisipkan Pesan", type=["jpg", "jpeg", "png"])
-    if st.button("Decode"):
-        if image_file is not None:
-            img = Image.open(io.BytesIO(image_file.read()))
-            
-            decoded_message = decode(img)
-            st.success(f"Pesan yang diekstrak: {decoded_message}")
-        else:
-            st.warning("Silakan pilih gambar terlebih dahulu.")
+            message += chr(int(hidden_bits[i], 2))
+            message = f'{message}'
+            hiddenmessage = message
+    # verifying the password
+    if password in message:
+        st.success(f"Hidden Message: {hiddenmessage[:-x]}")
+    else:
+        st.error("You entered the wrong password: Please Try Again")
+
+# Streamlit app
+def main():
+    st.title("$t3g0 - Image Steganography")
+    st.sidebar.title("Options")
+
+    options = ["Encode", "Decode"]
+    func = st.sidebar.selectbox("Select Operation", options)
+
+    if func == "Encode":
+        st.subheader("Encode")
+        src = st.file_uploader("Upload Source Image", type=["jpg", "jpeg", "png"])
+        message = st.text_area("Enter Message to Hide")
+        dest = st.text_input("Enter Destination Image Path")
+        password = st.text_input("Enter Password", type="password")
+
+        if st.button("Encode"):
+            if src is not None and message and dest and password:
+                encode(src, message, dest, password)
+            else:
+                st.error("Please fill in all the required fields.")
+
+    elif func == "Decode":
+        st.subheader("Decode")
+        src = st.file_uploader("Upload Source Image", type=["jpg", "jpeg", "png"])
+        password = st.text_input("Enter Password", type="password")
+
+        if st.button("Decode"):
+            if src is not None and password:
+                decode(src, password)
+            else:
+                st.error("Please fill in all the required fields.")
+
+if __name__ == "__main__":
+    main()
