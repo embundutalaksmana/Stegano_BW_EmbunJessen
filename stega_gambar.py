0781889429dace8ea5fcebbc0c79bc0655dc1b7c
embundutalaksmana <embun20ti@mahasiswa.pcr.ac.id>
2024-01-03 23:22:06 +0700

    26f commit

--- stega_gambar.py
+++ stega_gambar.py
@@ -1,60 +1,53 @@
 import streamlit as st
 from PIL import Image
 import io
-
-def encode_gray(img, message):
+def encode(img, message):
     pixels = list(img.getdata())
     binary_message = ''.join(format(ord(char), '08b') for char in message) + '1111111111111110'
     data_index = 0
     for i in range(len(pixels)):
-        pixel = pixels[i]
-        if data_index < len(binary_message):
-            new_pixel = pixel[0] & ~1 | int(binary_message[data_index]),
-            pixels[i] = new_pixel
-            data_index += 1
-    encoded_img = Image.new('L', img.size)
-    encoded_img.putdata([pixel[0] for pixel in pixels])
+        pixel = list(pixels[i])
+        for j in range(3):
+            if data_index < len(binary_message):
+                pixel[j] = pixel[j] & ~1 | int(binary_message[data_index])
+                data_index += 1
+        pixels[i] = tuple(pixel)
+    encoded_img = Image.new(img.mode, img.size)
+    encoded_img.putdata(pixels)
     return encoded_img
-
-def decode_gray(img):
+def decode(img):
     pixels = list(img.getdata())
     binary_message = ''
     for pixel in pixels:
-        binary_message += str(pixel & 1)  # Fix here: Remove [0] subscript
+        for j in range(3):
+            binary_message += str(pixel[j] & 1)
     delimiter_index = binary_message.find('1111111111111110')
     binary_message = binary_message[:delimiter_index]
     message = ''.join(chr(int(binary_message[i:i+8], 2)) for i in range(0, len(binary_message), 8))
     return message
-
 # Streamlit App
-st.title("Steganografi Gambar Grayscale dengan Algoritma LSB")
+st.title("Steganografi Gambar Hitam Putih dengan Algoritma LSB")
 option = st.selectbox("Pilih Operasi", ["Encode", "Decode"])
-
 if option == "Encode":
     st.subheader("Sisipkan Pesan ke dalam Gambar")
     image_file = st.file_uploader("Pilih Gambar", type=["jpg", "jpeg", "png"])
     message = st.text_area("Masukkan Pesan yang Akan Disisipkan")
-
     if st.button("Encode"):
         if image_file is not None and message != "":
             img = Image.open(io.BytesIO(image_file.read()))
-
-            encoded_img = encode_gray(img, message)
+            
+            encoded_img = encode(img, message)
             st.image(encoded_img, caption="Gambar Hasil Encode", use_column_width=True)
         else:
             st.warning("Silakan pilih gambar dan masukkan pesan terlebih dahulu.")
-
 elif option == "Decode":
     st.subheader("Ekstrak Pesan dari Gambar")
     image_file = st.file_uploader("Pilih Gambar yang Telah Disisipkan Pesan", type=["jpg", "jpeg", "png"])
-
     if st.button("Decode"):
         if image_file is not None:
             img = Image.open(io.BytesIO(image_file.read()))
-
-            decoded_message = decode_gray(img)
-            st.subheader("Pesan yang diekstrak:")
-            st.text(decoded_message)
+            
+            decoded_message = decode(img)
+            st.success(f"Pesan yang diekstrak: {decoded_message}")
         else:
             st.warning("Silakan pilih gambar terlebih dahulu.")
-
