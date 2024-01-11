148fb1ca349653999a5ee151d69b86bbebadecd0
embundutalaksmana <embun20ti@mahasiswa.pcr.ac.id>
2024-01-11 10:29:07 +0700

    27p commit

--- stega_gambar.py
+++ stega_gambar.py
@@ -28,35 +28,45 @@ def decode(img):
             break
         message += chr(int(byte, 2))
     return message
-
+def normalize_image(img):
+    normalized_img = img.convert("L")  # Konversi gambar ke skala abu-abu
+    return normalized_img
 # Streamlit App
-st.title("Steganografi Gambar Hitam Putih dengan Algoritma LSB")
-option = st.selectbox("Pilih Operasi", ["Encode", "Decode"])
 if option == "Encode":
     st.subheader("Sisipkan Pesan ke dalam Gambar")
     image_file = st.file_uploader("Pilih Gambar", type=["jpg","png"])
     message = st.text_area("Masukkan Pesan yang Akan Disisipkan")
-     # Pengecekan ukuran minimal 25KB
+    
+    # Pengecekan ukuran minimal 25KB
     if image_file is not None and image_file.size < 25 * 1024:  # 25 KB dalam byte
         st.warning("Ukuran gambar harus minimal 25KB.")
-    
+
     if st.button("Encode"):
-        
         if image_file is not None and message != "":
             img = Image.open(io.BytesIO(image_file.read()))
-            
-            encoded_img = encode(img, message)
+
+            # Normalisasi gambar sebelum encoding
+            normalized_img = normalize_image(img)
+
+            encoded_img = encode(normalized_img, message)
             st.image(encoded_img, caption="Gambar Hasil Encode", use_column_width=True)
         else:
             st.warning("Silakan pilih gambar dan masukkan pesan terlebih dahulu.")
+
+# ...
+
 elif option == "Decode":
     st.subheader("Ekstrak Pesan dari Gambar")
-    image_file = st.file_uploader("Pilih Gambar yang Telah Disisipkan Pesan", type=["png"])
+    image_file = st.file_uploader("Pilih Gambar yang Telah Disisipkan Pesan", type=["jpg","png"])
+    
     if st.button("Decode"):
         if image_file is not None:
             img = Image.open(io.BytesIO(image_file.read()))
-            
-            decoded_message = decode(img)
+
+            # Normalisasi gambar sebelum decoding
+            normalized_img = normalize_image(img)
+
+            decoded_message = decode(normalized_img)
             st.success(f"Pesan yang diekstrak: {decoded_message}")
         else:
             st.warning("Silakan pilih gambar terlebih dahulu.")
