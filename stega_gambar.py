c0bcb34ace56d1a488225838d38c2228c8f586fa
embundutalaksmana <embun20ti@mahasiswa.pcr.ac.id>
2024-01-11 07:55:33 +0700

    27d commit

--- stega_gambar.py
+++ stega_gambar.py
@@ -34,14 +34,21 @@ option = st.selectbox("Pilih Operasi", ["Encode", "Decode"])
 if option == "Encode":
     st.subheader("Sisipkan Pesan ke dalam Gambar")
     image_file = st.file_uploader("Pilih Gambar", type=["png"])
-    message = st.text_area("Masukkan Pesan yang Akan Disisipkan")
-    if st.button("Encode"):
-        if image_file is not None and message != "":
-            img = Image.open(io.BytesIO(image_file.read()))
-            
-            encoded_img = encode(img, message)
-            st.image(encoded_img, caption="Gambar Hasil Encode", use_column_width=True)
-            if st.button("Unduh Gambar Hasil Encode"):
+
+    # Pengecekan ukuran minimal 25KB
+    if image_file is not None and image_file.size < 25 * 1024:  # 25 KB dalam byte
+        st.warning("Ukuran gambar harus minimal 25KB.")
+    else:
+        message = st.text_area("Masukkan Pesan yang Akan Disisipkan")
+
+        if st.button("Encode"):
+            if image_file is not None and message != "":
+                img = Image.open(io.BytesIO(image_file.read()))
+                
+                encoded_img = encode(img, message)
+                st.image(encoded_img, caption="Gambar Hasil Encode", use_column_width=True)
+
+                if st.button("Unduh Gambar Hasil Encode"):
                     # Mengonversi gambar hasil menjadi byte
                     img_byte_array = io.BytesIO()
                     encoded_img.save(img_byte_array, format='PNG')
@@ -54,8 +61,9 @@ if option == "Encode":
                         file_name="encoded_image.png",
                         key="encoded_image"
                     )
-        else:
-            st.warning("Silakan pilih gambar dan masukkan pesan terlebih dahulu.")
+            else:
+                st.warning("Silakan pilih gambar dan masukkan pesan terlebih dahulu.")
+
 elif option == "Decode":
     st.subheader("Ekstrak Pesan dari Gambar")
     image_file = st.file_uploader("Pilih Gambar yang Telah Disisipkan Pesan", type=["png"])
